c9ea3448c44ca3168b4354225411e6331d0ce654
Nichen01 <64663395+Nichen01@users.noreply.github.com>
2021-02-13 02:22:43 +0800

    added google fonts for text

--- wikiscript.js
+++ wikiscript.js
@@ -6,6 +6,7 @@ fetch("wiki.json")
      document.querySelector("#wikislytherin").innerHTML = `<p style="font-size: 2rem;">${data[0].slytherin}</p>`
      document.querySelector("#wikiravenclaw").innerHTML = `<p style="font-size: 2rem;">${data[1].ravenclaw}</p>`
      document.querySelector("#wikihufflepuff").innerHTML = `<p style="font-size: 2rem;">${data[2].hufflepuff}</p>`
-     document.querySelector("#wikigryffindor").innerHTML = `<p style="font-size: 2rem;">${data[3].gryffindor}</p>`
+     document.querySelector("#wikigryfinddor").innerHTML = `<p style="font-size: 2rem;">${data[3].gryffindor}</p>`
      document.querySelector("#wikiwand").innerHTML = `<p style="font-size: 2rem;">${data[4].wand}</p>`
+     document.querySelector("#wandhistory").innerHTML = `<p style="font-size: 2rem;">${data[5].whistory}</p>`
  })
