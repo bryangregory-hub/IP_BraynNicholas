764338edecf84b1d4059689b7c8bbf02ee16bafe
Nichen01 <64663395+Nichen01@users.noreply.github.com>
2021-02-13 00:53:08 +0800

    Wiki nows displays using json file
    - wikiscript.js
    - wiki.json

---
+++ wikiscript.js
@@ -0,0 +1,11 @@
+//fetches the questions from the json file and displays them
+fetch("wiki.json")
+ .then(response => response.json())
+ .then(data => {
+     console.log(data)
+     document.querySelector("#wikislytherin").innerHTML = `<p style="font-size: 2rem;">${data[0].slytherin}</p>`
+     document.querySelector("#wikiravenclaw").innerHTML = `<p style="font-size: 2rem;">${data[1].ravenclaw}</p>`
+     document.querySelector("#wikihufflepuff").innerHTML = `<p style="font-size: 2rem;">${data[2].hufflepuff}</p>`
+     document.querySelector("#wikigryffindor").innerHTML = `<p style="font-size: 2rem;">${data[3].gryffindor}</p>`
+     document.querySelector("#wikiwand").innerHTML = `<p style="font-size: 2rem;">${data[4].wand}</p>`
+ })
