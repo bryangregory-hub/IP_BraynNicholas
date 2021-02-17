c7a8a5ab14a01490ef6c3655a4da07f85c4ccbc8
bryangregory-hub <s10207917@connect.np.edu.sg>
2021-02-17 09:57:35 +0800

    quiz json file attached with score and highscore

--- game.js
+++ game.js
@@ -15,10 +15,27 @@ fetch("quiz.json")
    return res.json();
 })
 .then(loadedQuestions=>{
-   console.log(loadedQuestions)
-   questions=loadedQuestions;
+   console.log(loadedQuestions);
+   questions=loadedQuestions.map(loadedQuestion=>{
+      const formattedQuestion={
+         questions:loadedQuestion.questions
+      }
+   const answerChoices=[...loadedQuestion.incorrect_answers];
+   formattedQuestion.answer=Math.floor(Math.random()*3+1);
+
+   answerChoices.splice(formattedQuestion.answer-1,0,loadedQuestion.correct_answer);
+
+   answerChoices.forEach((choice,index)=>
+   {
+      formattedQuestion["choice"+(index+1)]=choice;
+   })
+
+   return formattedQuestion;
+   
+   })
    startGame()
 })
+
 .catch(err=>{
    console.log(err);
 })
