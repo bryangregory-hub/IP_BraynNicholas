4e894cf27d0577e496a6a2779d8478055b5d7a6f
bryangregory-hub <s10207917@connect.np.edu.sg>
2021-02-18 15:06:29 +0800

    test

--- wikiscript.js
+++ wikiscript.js
@@ -8,11 +8,5 @@ fetch("wiki.json")
      document.querySelector("#wikihufflepuff").innerHTML = `<p style="font-size: 2rem;">${data[2].hufflepuff}</p>`
      document.querySelector("#wikigryfinddor").innerHTML = `<p style="font-size: 2rem;">${data[3].gryffindor}</p>`
      document.querySelector("#wikiwand").innerHTML = `<p style="font-size: 2rem;">${data[4].wand}</p>`
-     
-     document.querySelector("#wikipat").innerHTML = `<p style="font-size: 2rem;">${data[6].patronus}</p>`
-     document.querySelector("#pathistory").innerHTML = `<p style="font-size: 2rem;">${data[7].pathistory}</p>`
-     document.querySelector("#patcast").innerHTML = `<p style="font-size: 2rem;">${data[8].patcast}</p>`
-     document.querySelector("#housewiki").innerHTML = `<p style="font-size: 2rem;">${data[9].housewiki}</p>`
-     document.querySelector("#housesystem").innerHTML = `<p style="font-size: 2rem;">${data[10].housesystem}</p>`document.querySelector("#wandhistory").innerHTML = `<p style="font-size: 2rem;">${data[5].whistory}</p>`
+     document.querySelector("#wandhistory").innerHTML = `<p style="font-size: 2rem;">${data[5].whistory}</p>`
  })
- 
